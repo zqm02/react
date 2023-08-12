75d8318d7b152150684aeedd78d18075189d8051
zqm02 <101574538+zqm02@users.noreply.github.com>
2023-08-13 01:29:38 +0800

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,35 @@ import "./App.css";
 function App() {
   // const ele = <div>Hello,React~!!!</div>;
 
+  const name = "Hello,React";
+  const idName = "three";
+  const styles = {
+    fontSize: "24px",
+    color: "green",
+  };
+
   const ele = (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <>
+      <ul>
+        <li id="one">1</li>
+        <li id="two">2</li>
+        <li id={idName}>3</li>
+        <li>{20 * 2}</li>
+        <li>{name}</li>
+      </ul>
+
+      <ul
+        style={{
+          listStyle: "none",
+        }}
+      >
+        <li id="one">1</li>
+        <li id="two">2</li>
+        <li id={idName}>3</li>
+        <li style={styles}>{20 * 2}</li>
+        <li>{name}</li>
+      </ul>
+    </>
   );
   return ele;
 }
