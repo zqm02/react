1eaaa9104c2361cc65d99ad52f56be8d6abbc322
zqm02 <101574538+zqm02@users.noreply.github.com>
2023-08-14 23:45:32 +0800

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,7 +19,13 @@ function App() {
         <li>{20 * 2}</li>
         <li>{name}</li>
       </ul>
-
+      <ul>
+        <li id="one">1</li>
+        <li id="two">2</li>
+        <li id={idName}>3</li>
+        <li>{20 * 2}</li>
+        <li>{name}</li>
+      </ul>
       <ul
         style={{
           listStyle: "none",
